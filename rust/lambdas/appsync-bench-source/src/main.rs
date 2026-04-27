4bd0fc83a101fb77106eb5527f8ab5579878c343
Jérémie Rodon <jeremie.rodon@gmail.com>
2026-04-27 17:37:33 +0200

    Cleaning and updating deps for awssdk-instrumentation and dynamodb-facade

--- rust/lambdas/appsync-bench-source/src/main.rs
+++ rust/lambdas/appsync-bench-source/src/main.rs
@@ -5,4 +5,8 @@ pub use dynamodb_utils::appsync_types::*;
 
 // For the types and operations specific to this AppSync project, it uses the GraphQL schema file as a reference
 // for the Lambda handler and integration types, it uses generic (and opiniated) event structs
-lambda_appsync::appsync_lambda_main! ("graphql/schema.gql", exclude_appsync_types = true, dynamodb() -> aws_sdk_dynamodb::Client);
+lambda_appsync::appsync_lambda_main!(
+    "graphql/schema.gql",
+    exclude_appsync_types = true,
+    dynamodb() -> dynamodb_facade::Client
+);
